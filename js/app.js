ec15f77bbbdbffc8f6e6b04339eaf5e99ee4f15c
prestonevans <89256614+prestonevans@users.noreply.github.com>
2022-08-05 11:42:11 -0600

    created a rotating nav

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const open = document.querySelector('.open')
+const close = document.querySelector('.close')
+const menu = document.querySelector('.menu')
+const page = document.querySelector('.container')
+
+open.addEventListener('click',() => {
+    page.style.transform = 'rotate(-15deg)'
+    menu.style.transform = 'rotate(-90deg)'
+})
+
+close.addEventListener('click',() => {
+    page.style.transform = ''
+    menu.style.transform = ''
+})
